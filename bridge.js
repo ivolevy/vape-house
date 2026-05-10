c85e60b03c0ee53c1c89b8531cc56914d4267ead
ivan levy <ivo.levy03@gmail.com>
2026-05-10 12:07:34 -0300

    Manually construct Request in bridge.js to fix H3 export error

--- bridge.js
+++ bridge.js
@@ -1,8 +1,23 @@
-import { eventHandler, toWebRequest } from 'h3'
+import { eventHandler, getRequestURL, getRequestHeaders, readRawBody } from 'h3'
 import server from './dist/server/server.js'
 
 export default eventHandler(async (event) => {
-  const request = toWebRequest(event)
-  // Llamamos al método fetch del servidor de TanStack
+  const url = getRequestURL(event)
+  const headers = getRequestHeaders(event)
+  const method = event.method
+  
+  let body = null
+  if (method !== 'GET' && method !== 'HEAD') {
+    body = await readRawBody(event, false).catch(() => null)
+  }
+
+  const request = new Request(url, {
+    method,
+    headers,
+    body,
+    // Importante para evitar errores en algunas versiones de Node
+    duplex: body ? 'half' : undefined
+  })
+
   return server.fetch(request, {}, {})
 })
