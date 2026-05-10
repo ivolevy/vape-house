2a914a073485128edb29fafcf93c6fdcc92d2a8d
ivan levy <ivo.levy03@gmail.com>
2026-05-10 11:59:29 -0300

    Wrap server.fetch in H3 eventHandler in bridge.js

--- bridge.js
+++ bridge.js
@@ -1,1 +1,8 @@
-export { default } from './dist/server/server.js'
+import { eventHandler, toWebRequest } from 'h3'
+import server from './dist/server/server.js'
+
+export default eventHandler(async (event) => {
+  const request = toWebRequest(event)
+  // Llamamos al método fetch del servidor de TanStack
+  return server.fetch(request, {}, {})
+})
