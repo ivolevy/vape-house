ef14cdf1b2f36ae4d97731ebe5369773f16c91d3
ivan levy <ivo.levy03@gmail.com>
2026-05-10 11:07:31 -0300

    Force Nitro build using empty srcDir to satisfy Vercel

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,5 +2,12 @@ import { defineNitroConfig } from "nitropack/config";
 
 export default defineNitroConfig({
   preset: "vercel",
+  srcDir: ".nitro-empty-dir",
   compatibilityDate: "2024-04-03",
+  publicAssets: [
+    {
+      dir: "../dist/client",
+      maxAge: 31536000,
+    }
+  ],
 });
