7ee821310bcf7b1b5c72fcc83242f6eb7651494c
ivan levy <ivo.levy03@gmail.com>
2026-05-10 11:29:45 -0300

    Correct Nitro entry point to index.js

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,7 +3,7 @@ import { defineNitroConfig } from "nitropack/config";
 export default defineNitroConfig({
   preset: "vercel",
   compatibilityDate: "2024-04-03",
-  entry: "./dist/server/server.js",
+  entry: "./dist/server/index.js",
   output: {
     dir: ".output",
     serverDir: ".output/server",
